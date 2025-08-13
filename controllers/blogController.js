4aae9693bfb28bcf4f585151b50c9276c562626f
maninder <manindersolitaire@gmail.com>
2025-08-13 17:09:18 +0530

    Major Project Quick Blog App using MERN 8th Day Backend 2nd day

---
+++ controllers/blogController.js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js'
+
+export const addBlog =  async (req,res) => {
+    try {
+        const {title, subTitle, description , category , isPublished} = JSON.parse(req.body.blog)
+        const imageFile = req.file
+        // check if all fields are present
+
+        if(!title || !description || !category || !imageFile){
+            return res.json({success: false, message : "Missing required fields"})
+        }
+
+        const fileBuffer  =  fs.readFileSync(imageFile.path)
+
+        // Upload Image to ImageKit
+        const response = await imagekit.upload({
+            file : fileBuffer,
+            fileName:imageFile.originalname,
+            folder : "/blogs"
+        })
+
+        // Optimized Image ....
+    } catch (error) {
+        
+    }
+}
